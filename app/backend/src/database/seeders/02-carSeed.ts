8048cad57cec0d81f2f39d6dc9d0468d2136c6dd
gabgaleski <galeski35@gmail.com>
2024-03-18 15:32:22 -0300

    feat: Implementa banco de dados nas seeds com imagens dos carros e novos carros

--- app/backend/src/database/seeders/02-carSeed.ts
+++ app/backend/src/database/seeders/02-carSeed.ts
@@ -8,7 +8,7 @@ export default {
         status: 0,
         description: 'Carro ideal para a familia',
         brand: 'Renault',
-        images: '',
+        images: 'https://w7.pngwing.com/pngs/217/579/png-transparent-renault-kwid-car-dacia-logan-fiat-mobi-renault-compact-car-car-vehicle.png',
         value: 58.99
       },
       {
@@ -16,7 +16,7 @@ export default {
         status: 1,
         description: 'Carro ideal para a familia',
         brand: 'Fiat',
-        images: '',
+        images: 'https://production.autoforce.com/uploads/version/profile_image/9615/model_main_webp_comprar-trekking-1-0_26baeeef68.png.webp',
         value: 63.25
       },
       {
@@ -24,7 +24,7 @@ export default {
         status: 1,
         description: 'Carro',
         brand: 'Fiat',
-        images: '',
+        images: 'https://production.autoforce.com/uploads/version/profile_image/8015/comprar-drive-1-0_e7dee238c6.png',
         value: 69.86
       },
       {
@@ -32,8 +32,32 @@ export default {
         status: 1,
         description: 'Carro',
         brand: 'Hyundai',
-        images: '',
+        images: 'https://carwaysul.com.br/storage/app/uploads/public/638/0aa/3e5/6380aa3e5ecd5400406634.png',
         value: 86.55
+      },
+      {
+        name: 'Renault Logan 1.0',
+        status: 1,
+        description: 'Carro ideal para a familia',
+        brand: 'Renault',
+        images: 'https://brfrance.com.br/uploads/products/versions/renault-logan-branco-glacier.png',
+        value: 159.90
+      },
+      {
+        name: 'Peugeot 208 1.6',
+        status: 1,
+        description: 'Carro ideal para a familia',
+        brand: 'Peugeot',
+        images: 'https://peugeotlelac.com.br/wp-content/uploads/2022/03/style_azul.png',
+        value: 115.74
+      },
+      {
+        name: 'Peugeot 2008 1.6',
+        status: 1,
+        description: 'Carro ideal para a familia',
+        brand: 'Peugeot',
+        images: 'https://production.autoforce.com/uploads/version/profile_image/7617/comprar-allure-1-6_b8c2a72a3b.png',
+        value: 201.88
       },
     ], {});
   },
--- app/backend/src/database/seeders/04-carDetailsSeed.ts
+++ app/backend/src/database/seeders/04-carDetailsSeed.ts
@@ -27,6 +27,24 @@ export default {
         capacity: 5,
         gear: 'Manual',
       },
+      {
+        car_id: 5,
+        year: 2019,
+        capacity: 5,
+        gear: 'Automatico',
+      },
+      {
+        car_id: 6,
+        year: 2018,
+        capacity: 5,
+        gear: 'Automatico',
+      },
+      {
+        car_id: 7,
+        year: 2020,
+        capacity: 5,
+        gear: 'Manual',
+      },
     ], {});
   },
 
